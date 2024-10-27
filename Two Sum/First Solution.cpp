798ed00f7db12066a79ed347a1155bbf27334cf7
Artem2006Mishin <jock.fromrussia@gmail.com>
2024-10-27 13:29:59 +0300

    С помощью хеш-таблицы

---
+++ Two Sum/First Solution.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>       
+#include <vector> 
+#include <iostream>       
+#include <vector> 
+#include <unordered_map> 
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target)
+{
+    unordered_map <int, int> hash;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        hash[nums[i]] = i;
+    }
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int complement = target - nums[i];
+        if (hash.find(complement) != hash.end() &&
+            hash[complement] != i)
+        {
+            return { i, hash[complement] };
+        }
+    }
+
+    return {};
+}
+
+int main()
+{
+    vector<int> nums = { 3,3 };
+    int target = 6;
+
+    auto v = twoSum(nums, target);
+    cout << v[0] << " " << v[1];
+}
