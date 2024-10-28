cf5cf8202173105e9647117642ab37a98c436f0f
Artem2006Mishin <jock.fromrussia@gmail.com>
2024-10-28 22:05:14 +0300

    Через указатели

--- 3Sum/Second Solution.cpp
+++ 3Sum/Second Solution.cpp
@@ -8,13 +8,33 @@
 
 using namespace std;
 
-bool find3Numbers(vector<int>& arr, int sum)
+struct hashFunction
 {
+    size_t operator()(const vector<int>
+        & myVector) const
+    {
+        hash<int> hasher;
+        size_t answer = 0;
+
+        for (int i : myVector)
+        {
+            answer ^= hasher(i) + 0x9e3779b9 +
+                (answer << 6) + (answer >> 2);
+        }
+        return answer;
+    }
+};
+
+auto find3Numbers(vector<int>& arr)
+{
+    int sum = 0;
+    unordered_set<vector<int>, hashFunction> res;
+
     int n = arr.size();
     sort(arr.begin(), arr.end());
 
     for (int i = 0; i < n - 2; i++)
-    {     
+    {
         int left = i + 1;
         int right = n - 1;
 
@@ -23,10 +43,10 @@ bool find3Numbers(vector<int>& arr, int sum)
             int curr_sum = arr[i] + arr[left] + arr[right];
             if (curr_sum == sum)
             {
-                cout << arr[i] << " " << arr[left] << " " << arr[right];
-                return true;
+                vector<int> temp = { arr[i], arr[left], arr[right] };
+                res.insert(temp);
             }
-            
+
             if (sum > curr_sum)
             {
                 left++;
@@ -35,15 +55,22 @@ bool find3Numbers(vector<int>& arr, int sum)
             {
                 right--;
             }
-        }        
+        }
     }
-    return false;
+    return res;
 }
 
-int main() 
+int main()
 {
-    vector<int> arr = { 4, 12, 3, 1, 6, 9 };
-    int sum = 24;
+    vector<int> arr = { -1, 0, 1, 2, -1, -4 };
+    auto result = find3Numbers(arr);
+    for (auto n : result)
+    {
+        for (auto m : n)
+        {
+            cout << m << " ";
+        }
+        cout << endl;
+    }
 
-    find3Numbers(arr, sum);
 }
