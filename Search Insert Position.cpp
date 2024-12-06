d33812387fc293d7266be53767fc3adc2749d313
Artem2006Mishin <jock.fromrussia@gmail.com>
2024-12-06 22:37:45 +0300

    Create Search Insert Position.cpp
    https://www.youtube.com/watch?v=K-RYzDZkzCI

---
+++ Search Insert Position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int searchInsert(vector<int>& nums, int target) {
+	long int left = 0;
+	long int right = nums.size() - 1;
+	long int mid;
+
+	while (left < right)
+	{
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) {
+			return mid;
+		}
+		else if (nums[mid] < target) {
+			left = (mid + 1) > nums.size() - 1 ? nums.size() - 1 : mid + 1;
+		}
+		else {
+			right = (mid - 1 > 0) ? mid - 1 : 0;
+		}
+	}
+	if (target > nums[right])
+		return right + 1;
+	else
+		return right;
+    }
+};
