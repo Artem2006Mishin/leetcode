b8684f0453bb6ea4addd5be2df0f59d46c29a617
Artem2006Mishin <jock.fromrussia@gmail.com>
2024-12-07 13:24:30 +0300

    Create Sqrt.cpp

---
+++ Binary Search/Sqrt.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long int left = 1;
+        long int right = x;
+        long int mid;
+        long int res;
+        long int last;
+
+        while (left <= right) {
+            mid = (right - left) / 2 + left;
+            res = mid * mid;
+            if (res == x) {
+                return mid;
+            }
+            else if (res > x) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+                last = mid;
+            }
+        }
+        return last;
+    }
+};
